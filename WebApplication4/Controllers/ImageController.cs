e308bf89757c9347e6a643f89033bec67ad0496a
Abir-97 <mhabirkhan97@gmail.com>
2023-05-29 23:44:16 +0600

    first commit

---
+++ WebApplication4/Controllers/ImageController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    [Route("api/Demo")]
+    public class ImageController : Controller
+    {
+      
+        private IWebHostEnvironment _webHostEnvironment;
+        private IHttpContextAccessor httpContextAccessor;
+        public ImageController(IWebHostEnvironment webHostEnvironment, IHttpContextAccessor _httpContextAccessor)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            httpContextAccessor = _httpContextAccessor;
+        }
+
+        [Produces("application/json")]
+        [HttpPost("Upload")]
+        public IActionResult Upload(IFormFile[] files)
+        {
+            try
+            {
+                var baseUrl = httpContextAccessor.HttpContext.Request.Scheme + "://" + httpContextAccessor.HttpContext.Request.Host + httpContextAccessor.HttpContext.Request.PathBase;
+                var fileInformation = new List<FileUpload>();
+
+                foreach (var file in files)
+                {
+                    var path = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", file.FileName);
+                    using (var fileStream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    fileInformation.Add(new FileUpload
+                    {
+                        FileName = Path.Combine(baseUrl, "uploads", file.FileName),
+                        FileLength= file.Length,
+                    });
+                }
+
+                return Ok(fileInformation);
+              
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
+        }
+    }
+}
